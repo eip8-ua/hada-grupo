f8b3d66fa1645ab85ce365cfe073391c9d372ff6
edm33-ua <edm33@alu.ua.es>
2024-04-19 20:24:45 +0200

    48680346H Se han añadido las cabeceras de los métodos iniciales de la clase Informes

--- Library/CADInformes.cs
+++ Library/CADInformes.cs
@@ -8,5 +8,52 @@ namespace Library
 {
     class CADInformes
     {
+        string miConexion;
+        public string MiConexion { get { return miConexion; } set { miConexion = value; } }
+
+        public CADInformes()
+        {
+
+            //miConexion = ConfigurationManager.ConnectionStrings["miconexion"].ToString();
+        }
+
+        public void getTopProducts()
+        {
+
+        }
+
+        public void getTopClients()
+        {
+
+        }
+
+        public void getMostPopularProvince()
+        {
+
+
+        }
+
+        public void getProductIncome(ENProducto en)
+        {
+
+
+        }
+
+        public void getNumberOfSoldUnits(ENProducto en)
+        {
+
+
+        }
+
+        public void getNumberOfOrders(ENUsuario en)
+        {
+
+  
+        }
+
+        public void getAverageProductRating(ENProducto en)
+        {
+
+        }
     }
 }
