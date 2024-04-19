658690845d74e72c463bd3199af86f363b8bcf90
lap54-ua <lap54@alu.ua.es>
2024-04-19 14:24:38 +0200

    DNI: 53978387D; Declarados los métodos de ENUsuario y CADUsuario, e implementadas las llamadas desde el primero al segundo

--- Library/CADUsuario.cs
+++ Library/CADUsuario.cs
@@ -8,5 +8,33 @@ namespace Library
 {
     class CADUsuario
     {
+        private string conexion;
+        
+        public CADUsuario() { }
+        
+        public bool create(ENUsuario en)
+        {
+            return true;
+        }
+
+        public bool update(ENUsuario en)
+        {
+            return true;
+        }
+
+        public bool read(ENUsuario en)
+        {
+            return true;
+        }
+
+        public bool delete(ENUsuario en)
+        {
+            return true;
+        }
+
+        public bool admin(ENUsuario en)
+        {
+            return true;
+        }
     }
 }
