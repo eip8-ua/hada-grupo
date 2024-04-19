8f895eceb423a7db1246656b192120ef08a78402
Elias6970 <eliasiborra12@gmail.com>
2024-04-19 19:57:39 +0200

    DNI:46082179T; Completados las cabezeras de direccion y locker.

--- Library/CADLocker.cs
+++ Library/CADLocker.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.SqlClient;
 namespace Library
 {
     class CADLocker
     {
+        public string constring;
+        public SqlConnection connection;
+
+        public CADLocker()
+        {
+
+        }
+        public bool Create(ENLocker loc)
+        {
+
+        }
+        public bool Delete(ENLocker loc)
+        {
+
+        }
+        public bool Update(ENLocker loc)
+        {
+
+        }
+        public bool Read(ENLocker loc)
+        {
+
+        }
     }
 }
