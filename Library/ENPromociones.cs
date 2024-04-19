47ddb8eb8e6711f42bf186251921000e26696a89
edm33-ua <edm33@alu.ua.es>
2024-04-19 20:25:35 +0200

    48680346H Se han añadido las propiedades y las cabeceras pertenecientes a la clase Promociones

--- Library/ENPromociones.cs
+++ Library/ENPromociones.cs
@@ -8,5 +8,35 @@ namespace Library
 {
     class ENPromociones
     {
+        int miId;
+        float descuento;
+
+        public int MiId { get { return miId; } set { miId = value; } }
+
+        public float Descuento { get { return descuento; } set { descuento = value; } }
+
+        public ENPromociones() { }
+
+        public ENPromociones(int Yd, float desc) {
+
+            miId = Yd;
+            descuento = desc;
+        }
+
+        public List<ENPromociones> getPromociones() {
+
+            return null;
+        }
+
+         public DateTime getDisponibilidad() {
+
+            return new DateTime();
+         }
+
+        public int getNumberOfDaysRemaining() {
+
+            return 0;
+        }
+  
     }
 }
