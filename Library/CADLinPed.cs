9f92200970c10aba72fbbd19fc4fd72bce45cf78
dam55-ua <dam55@alu.ua.es>
2024-05-14 11:44:33 +0200

    DNI:06333401Y;Subida entidades Pedido y LinPed

--- Library/CADLinPed.cs
+++ Library/CADLinPed.cs
@@ -4,33 +4,136 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Library
 {
     class CADLinPed
     {
         private string constring;
-        private SqlConnection connection;
 
         public CADLinPed()
         {
-
+            constring = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
         }
         public bool Create(ENLinPed en)
         {
-            return true;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(constring))
+                {
+                    connection.Open();
+
+                    if (en.is_correct())
+                    {
+                        string query = "INSERT INTO Linea_pedido (id, cantidad, pedido, producto) VALUES (@id, @cantidad,@pedido, @producto)";
+                        SqlCommand cmd = new SqlCommand(query, connection);
+                        cmd.Parameters.AddWithValue("@id", en.IdLinPedido);
+                        cmd.Parameters.AddWithValue("@cantidad", en.Cantidad);
+                        cmd.Parameters.AddWithValue("@pedido", en.IdPedido);
+                        cmd.Parameters.AddWithValue("@producto", en.IdProducto);
+
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        return rowsAffected > 0;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error al crear la linea de pedido: " + e.Message);
+                return false;
+            }
         }
         public bool Update(ENLinPed en)
         {
-            return true;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(constring))
+                {
+                    connection.Open();
+
+                    if (en.is_correct())
+                    {
+                        string query = "UPDATE Pedido SET id = @id, cantidad = @cantidad,pedido=@pedido,producto=@producto WHERE id = @id";
+                        SqlCommand cmd = new SqlCommand(query, connection);
+                        cmd.Parameters.AddWithValue("@id", en.IdLinPedido);
+                        cmd.Parameters.AddWithValue("@cantidad", en.Cantidad);
+                        cmd.Parameters.AddWithValue("@pedido", en.IdPedido);
+                        cmd.Parameters.AddWithValue("@producto", en.IdProducto);
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        return rowsAffected > 0;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error al actualizar la linea de pedido: " + e.Message);
+                return false;
+            }
         }
         public bool Read(ENLinPed en)
         {
-            return true;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(constring))
+                {
+                    connection.Open();
+
+                    string query = "SELECT id, cantidad, pedido,producto FROM Linea_pedido WHERE id = @id";
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@id", en.IdLinPedido);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        en.Cantidad = reader.GetInt32(1); // El índice 1 corresponde a la columna "Cantidad"
+                        en.IdPedido = reader.GetInt32(2); // El índice 2 corresponde a la columna "IdPedido"
+                        en.IdProducto = reader.GetInt32(3);//El índice 3 corresponde a la columna "IdProducto"
+                        return true;
+                    }
+                    else
+                    {
+                        return false; // El pedido no fue encontrado
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error al leer la linea de pedido: " + e.Message);
+                return false;
+            }
         }
         public bool Delete(ENLinPed en)
         {
-            return true;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(constring))
+                {
+                    connection.Open();
+
+                    string query = "DELETE FROM Linea_pedido WHERE id = @id";
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@id", en.IdLinPedido);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error al eliminar el pedido: " + e.Message);
+                return false;
+            }
         }
     }
 }
