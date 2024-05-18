a01aef9cb91b9de2cc426e4094b4b86869d7e5d3
ab341-ua <ab341@alu.ua.es>
2024-05-18 17:21:49 +0200

    Y8485131P clase valoraciones CAD y EN hechas

--- Library/CADValoraciones.cs
+++ Library/CADValoraciones.cs
@@ -3,10 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Library
 {
-    class CADValoraciones
+    public class CADValoraciones
     {
+        private string constring;
+
+        public CADValoraciones()
+        {
+            constring = "Data Source=localhost;Initial Catalog=mydb;Integrated Security=True";
+        }
+
+        public DataTable getValoraciones()
+        {
+            SqlConnection conn = new SqlConnection(constring);
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Valora", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        public double GetMediaEvaluaciones(int productoId)
+        {
+            SqlConnection conn = new SqlConnection(constring);
+            SqlCommand cmd = new SqlCommand("SELECT AVG(Puntuacion) FROM Valora WHERE ProductoId = @ProductoId", conn);
+            cmd.Parameters.AddWithValue("@ProductoId", productoId);
+            conn.Open();
+            var result = cmd.ExecuteScalar();
+            conn.Close();
+            return result != DBNull.Value ? Convert.ToDouble(result) : 0;
+        }
+
+        public void ValorarProducto(int usuarioId, int productoId, int puntuacion, string descripcion)
+        {
+            SqlConnection conn = new SqlConnection(constring);
+            SqlCommand cmd = new SqlCommand("IF EXISTS (SELECT 1 FROM Valora WHERE UsuarioId = @UsuarioId AND ProductoId = @ProductoId) " +
+                                             "UPDATE Valora SET Puntuacion = @Puntuacion, Descripcion = @Descripcion WHERE UsuarioId = @UsuarioId AND ProductoId = @ProductoId " +
+                                             "ELSE " +
+                                             "INSERT INTO Valora (UsuarioId, ProductoId, Puntuacion, Descripcion) VALUES (@UsuarioId, @ProductoId, @Puntuacion, @Descripcion)", conn);
+            cmd.Parameters.AddWithValue("@UsuarioId", usuarioId);
+            cmd.Parameters.AddWithValue("@ProductoId", productoId);
+            cmd.Parameters.AddWithValue("@Puntuacion", puntuacion);
+            cmd.Parameters.AddWithValue("@Descripcion", descripcion);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public DataTable ObtenerValoracionesAnteriores(int productoId)
+        {
+            SqlConnection conn = new SqlConnection(constring);
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Valora WHERE ProductoId = @ProductoId", conn);
+            da.SelectCommand.Parameters.AddWithValue("@ProductoId", productoId);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
     }
 }
--- Library/ENValoraciones.cs
+++ Library/ENValoraciones.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Library
 {
     public class ENValoraciones
     {
+        public int UsuarioId { get; set; }
+        public int ProductoId { get; set; }
+        public int Puntuacion { get; set; }
+        public string Descripcion { get; set; }
+
+        // Relaciones
+        public ENValoracionUsuario Usuario { get; set; }
+        public ENValoracionProducto Producto { get; set; }
+
+        public ENValoraciones()
+        {
+        }
+    }
+
+    public class ENValoracionUsuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        // Otras propiedades relevantes
+
+        public ENValoracionUsuario()
+        {
+        }
+    }
+
+    public class ENValoracionProducto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        // Otras propiedades relevantes
+
+        public ENValoracionProducto()
+        {
+        }
     }
 }
