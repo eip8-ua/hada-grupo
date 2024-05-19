7b0d5685a53c3b19ffa3949400651b30113ecdf0
dma72-ua <dma72@alu.ua.es>
2024-05-19 19:25:54 +0200

    DNI:20526351R; Añadidos el CAD y EN de Carrito_de

---
+++ Library/CADCarritoDe.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Library
+{
+    class CADCarritoDe
+    {
+        public string connectionString;
+
+        /// <summary>
+        /// Constructor vacío
+        /// </summary>
+        public CADCarritoDe()
+        {
+            this.connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+
+        /// <summary>
+        /// Método que inserta una Linea de Carrito en la BD
+        /// </summary>
+        /// <param name="en">EN con los datos de la Linea de Carrito</param>
+        /// <returns></returns>
+        public bool Create(ENCarritoDe en)
+        {
+            string insertQuery = "INSERT INTO Carrito_de (usuario, carrito, producto) VALUES (@UserId, @CartId)";
+            int userId = en.Usuario;
+            int cartId = en.Carrito;
+
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(insertQuery, connection))
+                    {
+                      
+                        command.Parameters.AddWithValue("@UserId", userId);
+                        command.Parameters.AddWithValue("@CartId", cartId);
+                        
+                        connection.Open();
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            Console.WriteLine("Line item added to shopping cart successfully!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Failed to add line item to shopping cart.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error creating line item: {ex.Message}");
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Método que actualiza una Linea de Carrito en la BD
+        /// </summary>
+        /// <param name="en">EN con los datos de la Linea de Carrito</param>
+        /// <returns></returns>
+        public bool Update(ENCarritoDe en)
+        {
+            string updateQuery = "UPDATE Carrito_de SET carrito = @CartId WHERE id = @UserId";
+            int userId = en.Usuario;
+            int cartId = en.Carrito;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(updateQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@UserId", userId);
+                        command.Parameters.AddWithValue("@CartId", cartId);
+
+                        connection.Open();
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            Console.WriteLine("Line item updated successfully!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Line item not found.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error updating line item: {ex.Message}");
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Método que lee los datos de una Linea de Carrito de la BD
+        /// </summary>
+        /// <param name="en">EN con los datos de la Linea de Carrito</param>
+        /// <returns></returns>
+        public bool Read(ENCarritoDe en)
+        {
+
+            string selectQuery = "SELECT * FROM Carrito_de WHERE id = @UserId";
+            int userId = en.Usuario;
+            int cartId = en.Carrito;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(selectQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@UserId", userId);
+
+                        connection.Open();
+
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"ID: ");
+                        }
+
+                        if (!reader.HasRows)
+                        {
+                            Console.WriteLine("No line items found in the shopping cart.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading line items: {ex.Message}");
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Método que elimina una Linea de Carrito de la BD
+        /// </summary>
+        /// <param name="en">EN con los datos de la Linea de Carrito</param>
+        /// <returns></returns>
+        public bool Delete(ENCarritoDe en)
+        {
+            string deleteQuery = "DELETE FROM Carrito_de WHERE usuario = @UserId";
+            int userId = en.Usuario;
+            int cartId = en.Carrito;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(deleteQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@UserId", userId);
+
+                        connection.Open();
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            Console.WriteLine("Line item deleted successfully!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Line item not found.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error deleting line item: {ex.Message}");
+            }
+            return true;
+        }
+    }
+}
---
+++ Library/ENCarritoDe.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class ENCarritoDe
+    {
+        private int _usuario;
+        private int _carrito;
+
+        public int Usuario { set { _usuario = value; } get { return _usuario; } }
+        public int Carrito { set { _carrito = value; } get { return _carrito; } }
+       
+        /// <summary>
+        /// Constructor vacío
+        /// </summary>
+        public ENCarritoDe() { }
+
+        /// <summary>
+        /// Constructor con los todos los datos de la linea de Carrito
+        /// </summary>
+        /// <param name="usuario"></param>
+        /// <param name="cantidad"></param>
+        public ENCarritoDe(int usuario, int carrito)
+        {
+            Usuario = usuario;
+            Carrito = carrito;
+        }
+        /// <summary>
+        /// Método que crea una linea de carrito con los datos almacenados en el objeto EN
+        /// </summary>
+        /// <returns>True si lo ha realizado con éxito; False si no</returns>
+        public bool Create()
+        {
+            CADCarritoDe cadCarritoDe = new CADCarritoDe();
+            return cadCarritoDe.Create(this);
+        }
+
+        /// <summary>
+        /// Método que actualiza la linea de carrito 
+        /// </summary>
+        /// <returns>True si lo ha realizado con éxito; False si no</returns>
+        public bool Update()
+        {
+            CADCarritoDe cadCarritoDe = new CADCarritoDe();
+            return cadCarritoDe.Update(this);
+        }
+        /// <summary>
+        /// Método que extrae los datos de la linea de carrito 
+        /// </summary>
+        /// <returns>True si lo ha realizado con éxito; False si no</returns>
+        public bool Read()
+        {
+            CADCarritoDe cadCarritoDe = new CADCarritoDe();
+            return cadCarritoDe.Read(this);
+        }
+        /// <summary>
+        /// Método que elimina la linea de carrito 
+        /// </summary>
+        /// <returns>True si lo ha realizado con éxito; False si no</returns>
+        public bool Delete()
+        {
+            CADCarritoDe cadCarritoDe = new CADCarritoDe();
+            return cadCarritoDe.Delete(this);
+        }
+    }
+}
