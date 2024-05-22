7a1a35b34b76aabaef01b3f1c91fc3722dd2f486
scm136 <scm136@alu.ua.es>
2024-05-22 12:34:41 +0200

    24435837Q, merge

--- FrontEnd/admin_pedidos.aspx.cs
+++ FrontEnd/admin_pedidos.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Library;
 
 namespace FrontEnd
 {
@@ -11,7 +12,29 @@ namespace FrontEnd
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                ENPedido ped = new ENPedido(0, new DateTime(2000, 10, 01), 0);
+
+                rptListPedidos.DataSource = ped.listPedidos();
+                rptListPedidos.DataBind();
+            }
+
 
         }
+
+        protected void EliminarPedido_click(object sender, EventArgs e)
+        {
+            string idStr = Request.QueryString["index"];
+
+            // Convertir el valor a un número entero (si es necesario)
+            if (int.TryParse(idStr, out int index))
+            {
+                ENPedido ped = new ENPedido(index, new DateTime(2000, 10, 01), 0);
+
+                ped.Delete();
+            }
+        }
     }
+
 }
