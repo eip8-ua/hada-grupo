058ad8324d5eab5b5455f491ca2add8408524e0f
ab341-ua <ab341@alu.ua.es>
2024-05-23 21:08:03 +0200

    Y8485131P modificando pedidos.aspx

--- FrontEnd/pedidos.aspx.cs
+++ FrontEnd/pedidos.aspx.cs
@@ -1,17 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using Library;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace FrontEnd
 {
-    public partial class pedidos : System.Web.UI.Page
+    public partial class pedidos : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                CargarPedidos();
+            }
+        }
 
+        private void CargarPedidos()
+        {
+            int userId = ObtenerIdUsuario();
+
+            if (userId > 0)
+            {
+                List<ENPedido> pedidos = ENPedido.ReadAll();
+
+                var userPedidos = pedidos.FindAll(p => p.IdUsuario == userId);
+
+                if (userPedidos.Count > 0)
+                {
+                    OrdersGridView.DataSource = userPedidos;
+                    OrdersGridView.DataBind();
+                    NoOrdersLabel.Visible = false;
+                }
+                else
+                {
+                    OrdersGridView.Visible = false;
+                    NoOrdersLabel.Text = "No se ha realizado ningún pedido aún.";
+                    NoOrdersLabel.Visible = true;
+                }
+            }
+            else
+            {
+                // Manejar el caso en que no se encuentre el ID del usuario
+                // Por ejemplo, redirigir a la página de login
+                Response.Redirect("~/login.aspx");
+            }
+        }
+
+        private int ObtenerIdUsuario()
+        {
+            if (Session["UserID"] != null)
+            {
+                return (int)Session["UserID"];
+            }
+            else
+            {
+                return -1; // Usuario no autenticado
+            }
         }
     }
 }
